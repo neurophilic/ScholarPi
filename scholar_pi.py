f81ecc78c71e59dd955d8caddac94c7ed7a82f4f
neurophilic <167536367+neurophilic@users.noreply.github.com>
2026-07-19 20:34:08 +0200

    Update scholar_pi.py

--- scholar_pi.py
+++ scholar_pi.py
@@ -291,14 +291,15 @@ def generate_interactive_bubble_chart(scope, user_id):
     net = Network(height='600px', width='100%', bgcolor='#ffffff', font_color='#2c3e50', notebook=False)
     
     # --- ADJUSTED PHYSICS FOR MORE SPACE ---
-    physics_options = """
+  physics_options = """
     {
       "physics": {
         "forceAtlas2Based": {
-          "gravitationalConstant": 0,
-          "centralGravity": 0.01,
-          "springLength": 50,
-          "avoidOverlap": 0.5
+          "gravitationalConstant": -200,
+          "centralGravity": 0.05,
+          "springLength": 200,
+          "springConstant": 0.08,
+          "avoidOverlap": 1.0
         },
         "solver": "forceAtlas2Based"
       }
